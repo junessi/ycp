33fa6d8a6337b7cf449b6dcbfac68ba3d49b89dc
Junlin Huang <cricketlong@gmail.com>
2021-06-10 00:21:08 +0200

    make download work

--- ycp.py
+++ ycp.py
@@ -6,13 +6,19 @@ import sys
 
 class MusicList(npyscreen.ActionForm):
     def create(self):
-       music_list = self.load_music_list()
+       self.music_list = self.load_music_list()
        self.musicList = self.add(npyscreen.GridColTitles,
-                                 col_titles = ["artist", "title", "link"],
-                                 columns = 3,
+                                 col_titles = ["{0}{1}link".format("artist".ljust(32), "title".ljust(32))],
+                                 columns = 1,
                                  column_margin = 0,
-                                 values = [[item["artist"], item["title"], item["link"]] for item in music_list["items"]],
+                                 values = [["{0}{1}{2}".format(item["artist"].ljust(32),
+                                                               item["title"].ljust(32),
+                                                               item["link"])]
+                                           for item in self.music_list["items"]],
                                  select_whole_line = True)
+
+       self.OK_BUTTON_TEXT = "Exit"
+       self.CANCEL_BUTTON_TEXT = "Download"
 
     def load_music_list(self):
         list_file = "ycp.json"
@@ -24,31 +30,28 @@ class MusicList(npyscreen.ActionForm):
     def on_ok(self):
         sys.exit(0)
 
+    def on_cancel(self):
+        audio_format = "mp3"
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': audio_format
+            }]
+        }
+
+        for item in self.music_list["items"]:
+            print(item)
+            ydl_opts["outtmpl"] = "{0} - {1}.%(ext)s".format(item["artist"], item["title"], audio_format)
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([item["link"]])
+
+
 class MyApplication(npyscreen.NPSAppManaged):
    def onStart(self):
        self.addForm('MAIN', MusicList)
-       # A real application might define more forms here.......
 
 if __name__ == '__main__':
     TestApp = MyApplication().run()
 
-    """
-    f = open("ycp.json", "r")
-    file_dict = json.load(f)
-
-    audio_format = "mp3"
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': audio_format
-        }]
-    }
-
-    for item in file_dict["items"]:
-        print(item)
-        ydl_opts["outtmpl"] = "{0} - {1}.%(ext)s".format(item["artist"], item["title"], audio_format)
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([item["link"]])
-    """
 
