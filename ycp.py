eb8bdb4a61d1c8777ad71d3eb64837a63514941a
Junlin Huang <cricketlong@gmail.com>
2021-06-17 01:47:43 +0200

    add feature: edit and save item

--- ycp.py
+++ ycp.py
@@ -31,6 +31,24 @@ class SelectableRow(urwid.WidgetWrap):
 
         return key
 
+    def get_music_data(self, pos):
+        if pos >= 0:
+            (artist_col, _) = self._columns.contents[0]
+            (title_col, _)  = self._columns.contents[1]
+            (link_col, _)   = self._columns.contents[2]
+            return {"artist": artist_col.text,
+                    "title":  title_col.text,
+                    "link":   link_col.text}
+
+    def update_data(self, data):
+        if len(data) == len(self.contents):
+            (artist_col, _) = self._columns.contents[0]
+            artist_col.set_text(data[0])
+            (title_col, _) = self._columns.contents[1]
+            title_col.set_text(data[1])
+            (link_col, _) = self._columns.contents[2]
+            link_col.set_text(data[2])
+
 class MyListBox(urwid.ListBox):
     def __init__(self, body, on_focus_change=None):
         super().__init__(body)
@@ -58,17 +76,18 @@ class MyListBox(urwid.ListBox):
 class MusicList(urwid.PopUpLauncher):
     signals = ['close']
     def __init__(self, on_edit_callback = None):
+        self.music_list_file = "ycp.json"
         self.on_edit_callback = on_edit_callback
         self.HEADERS = ["Artist", "Title", "Link"]
         music_list = self.read_music_list()
         self.ENTRIES = [[item["artist"], item["title"], item["link"]] for item in music_list["items"]]
         column_headers = urwid.AttrMap(urwid.Columns([urwid.Text(c) for c in self.HEADERS]), "column_headers")
 
-        rows = []
+        self.selectable_rows = []
         for entry in self.ENTRIES:
-            row = SelectableRow(entry, self.on_edit_callback)
-            rows.append(row)
-        self.content = urwid.SimpleFocusListWalker(rows)
+            row = SelectableRow(entry)
+            self.selectable_rows.append(row)
+        self.content = urwid.SimpleFocusListWalker(self.selectable_rows)
         self.listbox = MyListBox(self.content)
 
         # Get terminal dimensions
@@ -90,18 +109,21 @@ class MusicList(urwid.PopUpLauncher):
         return pop_up
 
     def add_music(self, artist, title, link):
-        self.content.append(SelectableRow([artist, title, link], self.on_edit_callback))
+        self.content.append(SelectableRow([artist, title, link]))
 
-    def get_music(self, index):
-        return {"artist": self.ENTRIES[index][0],
-                "title": self.ENTRIES[index][1],
-                "link": self.ENTRIES[index][2]}
+    def edit_music(self, pos, artist, title, link):
+        if pos >= 0:
+            item = self.selectable_rows[pos]
+            item.update_data([artist, title, link])
+
+    def get_music(self, pos):
+        return self.selectable_rows[pos].get_music_data(pos)
 
     def get_pop_up_parameters(self):
-        return {'left':0, 'top':1, 'overlay_width':32, 'overlay_height':7}
+        return {'left': 0, 'top': 1, 'overlay_width': 32, 'overlay_height': 7}
 
     def read_music_list(self):
-        f = open("ycp.json", "r")
+        f = open(self.music_list_file, "r")
         return json.load(f)
 
     def get_layout(self):
@@ -126,14 +148,15 @@ class App(object):
         self.title_edit = urwid.Edit(u"Title: ", u"")
         self.link_edit = urwid.Edit(u"Link: ", u"")
         self.item_editor = [self.artist_edit, self.title_edit, self.link_edit]
-        self.save_button = urwid.Button(u"Save", lambda button : self.add_music(self.artist_edit.get_edit_text(),
-                                                                                self.title_edit.get_edit_text(),
-                                                                                self.link_edit.get_edit_text()
-                                                                                ))
+        self.save_button = urwid.Button(u"Save", lambda button : self.save_music(self.artist_edit.get_edit_text(),
+                                                                                 self.title_edit.get_edit_text(),
+                                                                                 self.link_edit.get_edit_text()
+                                                                                 ))
         self.cancel_button = urwid.Button(u"Cancel", lambda button : self.handle_input('q'))
         self.item_editor.append(urwid.Columns([self.save_button, self.cancel_button]))
 
         self.adding_music = False
+        self.editing_music = False
         self.display_edit = False
         self.view = urwid.SimpleFocusListWalker([])
         self.view.append(urwid.Pile(self.music_list.get_layout()))
@@ -149,25 +172,30 @@ class App(object):
         """
 
     def on_edit_item(self):
-        self.handle_input('a')
+        self.handle_input('e')
 
     def handle_input(self, key):
         if key in ('q', 'Q', 'esc'):
-            if self.adding_music:
-                self.display_edit_dialog(False)
+            if self.adding_music or self.editing_music:
                 self.adding_music = False
+                self.editing_music = False
+                self.display_edit_dialog(False)
             else:
                 self.exit()
         elif key in ('a', 'A'):
-            self.display_edit_dialog(True)
             self.adding_music = True
+            self.display_edit_dialog(True)
         elif key in ('e', 'E'):
-            pos = self.music_list.get_cursor_position()
-            data = self.music_list.get_music(pos)
+            self.editing_music = True
+            self.editing_music_pos = self.music_list.get_cursor_position()
+            data = self.music_list.get_music(self.editing_music_pos)
             self.display_edit_dialog(True, data)
 
-    def add_music(self, artist, title, link):
-        self.music_list.add_music(artist, title, link)
+    def save_music(self, artist, title, link):
+        if self.adding_music:
+            self.music_list.add_music(artist, title, link)
+        elif self.editing_music:
+            self.music_list.edit_music(self.editing_music_pos, artist, title, link)
         self.handle_input('q')
 
     def display_edit_dialog(self, display, data = None):
@@ -179,6 +207,9 @@ class App(object):
                 self.link_edit.set_edit_text(data["link"])
             self.view.append(urwid.Pile(self.item_editor + self.music_list.get_layout()))
         else:
+            self.artist_edit.set_edit_text("")
+            self.title_edit.set_edit_text("")
+            self.link_edit.set_edit_text("")
             self.view.append(urwid.Pile(self.music_list.get_layout()))
 
     def exit(self):
